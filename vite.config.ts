4e42dbeb9b48cebadfc24bf74b7154ce20aedccd
orbitpranjali-source <orbitpranjali@gmail.com>
2026-01-22 17:43:50 +0530

    Changes in whole website

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/OrbitEngineering-group/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  return {
+    plugins: [react()],
+    base: isProduction ? '/OrbitEngineering-group/' : '/',
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+  };
 })
 
